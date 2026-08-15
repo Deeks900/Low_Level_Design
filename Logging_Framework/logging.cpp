4d7d3aaf1308f8a4be5b6c3afff26de38c7e006e
Deeksha Sharma <deekshasharma2022@gmail.com>
2026-08-16 03:50:48 +0530

    Part2-AsyncLog_Completed

--- Logging_Framework/logging.cpp
+++ Logging_Framework/logging.cpp
@@ -3,10 +3,15 @@
 #include <mutex>
 #include <thread>
 #include <memory>
+#include <condition_variable>
 
 using namespace std;
 
-// enum class for different log levels
+
+// ============================================================
+// LOG LEVEL
+// ============================================================
+
 enum class LogLevel{
     DEBUG,
     INFO, 
@@ -15,8 +20,11 @@ enum class LogLevel{
     FATAL
 };
 
-// Now we will be creating Log Message class
-// Single Responsibility Principle satisfied here, as this class is only responsible for creating log messages
+
+// ============================================================
+// LOG MESSAGE
+// ============================================================
+
 class LogMessage{
     private:
         LogLevel level;
@@ -24,7 +32,7 @@ class LogMessage{
         time_t timestamp;
 
     public:
-        // constructor
+
         LogMessage(LogLevel level, string message) {
             this->level = level;
             this->message = message;
@@ -44,173 +52,452 @@ class LogMessage{
         }
 };
 
-// Now let's move to Formatter, We will be creating an abstract class
+
+// ============================================================
+// FORMATTER
+// ============================================================
+
 class LogFormatter{
     public:
+
         virtual ~LogFormatter() {}
+
         virtual string formatLogMessage(LogMessage& logMessage) = 0;
 };
 
-// Now we will be creating a concrete class for formatter
+
+// ============================================================
+// DEFAULT FORMATTER
+// ============================================================
+
 class defaultFormatter : public LogFormatter { 
     public:
-        // Implement that pure virtual function
+
         string formatLogMessage(LogMessage& logMessage) override { 
-            // formatting of message will be done
             return logMessage.getMessage();
         }
 };
 
-// Now we will be creating an abstract class Appender 
+
+// ============================================================
+// APPENDER
+// ============================================================
+
 class Appender {
     public:
+
         virtual ~Appender(){}
+
         virtual void append(string formattedLogMessage) = 0;
 };
 
-// Now we will be creating concrete classes for appender 
+
+// ============================================================
+// FILE APPENDER
+// ============================================================
+
 class fileAppender : public Appender {
     private:
-        mutex mtx; // Mutex for thread safety
+
+        mutex mtx;
 
     public:
+
         void append(string logMessage) override {
+
             lock_guard<mutex> lock(mtx);
 
-            // here we will write the log message to a file
+            // Write logMessage to file
+
             return;
         }
 };
 
+
+// ============================================================
+// CONSOLE APPENDER
+// ============================================================
+
 class consoleAppender : public Appender {
     private:
-        mutex mtx; // Mutex for thread safety
+
+        mutex mtx;
 
     public:
+
         void append(string logMessage) override {
 
             lock_guard<mutex> lock(mtx);
 
-            // here we will write the log message to console
             cout << logMessage << endl;
         }
 };
 
-// Now we will be making the logger class which will be the orchestrating class
+
+// ============================================================
+// LOGGER
+// ============================================================
+
 class Logger {
+
     private:
+
         string name;
         LogLevel level;
 
-        // shared_ptr instead of raw pointer
+
+        // ----------------------------------------------------
+        // FORMATTER
+        // ----------------------------------------------------
+
         shared_ptr<LogFormatter> formatter;
 
-        // shared_ptr instead of raw pointers
+
+        // ----------------------------------------------------
+        // APPENDERS
+        // ----------------------------------------------------
+
         vector<shared_ptr<Appender>> appenders;
 
-        // Protects the appenders vector
         mutex appendersMutex;
 
+
+        // ----------------------------------------------------
+        // LOG QUEUE
+        // ----------------------------------------------------
+
+        queue<string> logQueue;
+
+        mutex queueMutex;
+
+        condition_variable cv;
+
+
+        // ----------------------------------------------------
+        // WORKER THREAD
+        // ----------------------------------------------------
+
+        thread workerThread;
+
+
+        // ----------------------------------------------------
+        // SHUTDOWN FLAG
+        // ----------------------------------------------------
+
+        bool running = true;
+
+
+        // ====================================================
+        // WORKER FUNCTION
+        // ====================================================
+
+        void processLogs(){
+
+            while(true){
+
+                string message;
+
+
+                // ------------------------------------------------
+                // Take one message from queue
+                // ------------------------------------------------
+
+                {
+                    unique_lock<mutex> lock(queueMutex);
+
+
+                    /*
+                        Worker sleeps until:
+
+                        1. Queue has a log
+                                   OR
+                        2. Logger is shutting down
+                    */
+
+                    cv.wait(lock, [this] {
+
+                        return !logQueue.empty() || !running;
+
+                    });
+
+
+                    /*
+                        If Logger is shutting down
+                        AND there are no logs remaining,
+                        worker can exit.
+                    */
+
+                    if(!running && logQueue.empty()){
+
+                        break;
+                    }
+
+
+                    // Take oldest log
+                    message = logQueue.front();
+
+                    logQueue.pop();
+
+                } // queueMutex released here
+
+
+                // ------------------------------------------------
+                // Take snapshot of appenders
+                // ------------------------------------------------
+
+                vector<shared_ptr<Appender>> currentAppenders;
+
+                {
+                    lock_guard<mutex> lock(appendersMutex);
+
+                    currentAppenders = appenders;
+
+                } // appendersMutex released here
+
+
+                // ------------------------------------------------
+                // Send log to appenders
+                // ------------------------------------------------
+
+                for(auto appender : currentAppenders){
+
+                    appender->append(message);
+                }
+            }
+        }
+
+
     public:
+
+
+        // ====================================================
+        // CONSTRUCTOR
+        // ====================================================
+
         Logger(
             string name,
             LogLevel level,
             shared_ptr<LogFormatter> formatter
-        ) {
+        ){
+
             this->name = name;
             this->level = level;
             this->formatter = formatter;
+
+
+            // Start worker thread
+            workerThread = thread(
+                &Logger::processLogs,
+                this
+            );
         }
 
-        // No destructor needed for formatter or appenders.
-        // shared_ptr automatically manages their lifetime.
+
+        // ====================================================
+        // DESTRUCTOR
+        // ====================================================
+
+        ~Logger(){
+
+            /*
+                Tell worker that Logger is shutting down.
+            */
+
+            {
+                lock_guard<mutex> lock(queueMutex);
+
+                running = false;
+            }
+
+
+            /*
+                Worker may currently be sleeping
+                on condition_variable.
+
+                Wake it up.
+            */
+
+            cv.notify_one();
+
+
+            /*
+                Wait for worker to finish
+                processing remaining logs.
+            */
+
+            workerThread.join();
+        }
+
+
+        // ====================================================
+        // ADD APPENDER
+        // ====================================================
 
         void addAppender(shared_ptr<Appender> appender){
 
             lock_guard<mutex> lock(appendersMutex);
 
-            this->appenders.push_back(appender);
+            appenders.push_back(appender);
         }
+
+
+        // ====================================================
+        // LOG
+        // ====================================================
 
         void log(LogLevel level, string message){
 
             if(level < this->level){
-                return; // Ignore messages below logger's level
+
+                return;
             }
 
+
+            // ------------------------------------------------
+            // Create LogMessage
+            // ------------------------------------------------
+
             LogMessage logMessage(level, message);
+
+
+            // ------------------------------------------------
+            // Format message
+            // ------------------------------------------------
 
             string formatLog =
                 formatter->formatLogMessage(logMessage);
 
-            // Take a snapshot of the appenders
-            vector<shared_ptr<Appender>> currentAppenders;
+
+            // ------------------------------------------------
+            // Put message into queue
+            // ------------------------------------------------
 
             {
-                lock_guard<mutex> lock(appendersMutex);
+                lock_guard<mutex> lock(queueMutex);
 
-                currentAppenders = appenders;
-            } // appendersMutex released here
 
-            // No Logger vector lock now
-            for(auto appender : currentAppenders){
-                appender->append(formatLog);
-            }
+                /*
+                    Logger may have started shutting down.
+
+                    Don't accept new logs after that.
+                */
+
+                if(!running){
+
+                    return;
+                }
+
+
+                logQueue.push(formatLog);
+
+            } // queueMutex released here
+
+
+            // ------------------------------------------------
+            // Wake worker
+            // ------------------------------------------------
+
+            cv.notify_one();
         }
 
-        // Now the functions user will be making use of to log messages
+
+        // ====================================================
+        // USER FACING FUNCTIONS
+        // ====================================================
+
         void debug(string message){
+
             log(LogLevel::DEBUG, message);
         }
 
         void info(string message){
+
             log(LogLevel::INFO, message);
         }
 
         void warn(string message){
+
             log(LogLevel::WARN, message);
         }
 
         void error(string message){
+
             log(LogLevel::ERROR, message);
         }
 
         void fatal(string message){
+
             log(LogLevel::FATAL, message);
         }
 };
 
-// Now implementing the logger Manager
+
+// ============================================================
+// LOGGER MANAGER
+// ============================================================
+
 class LoggerManager { 
+
     private:
+
         unordered_map<string, Logger*> loggers;
 
-        LoggerManager() {} // For singleton pattern, private constructor
+
+        // Private constructor
+        LoggerManager() {}
+
 
     public:
-        // Can only be accessed through class method, singleton pattern
+
+
+        // ====================================================
+        // SINGLETON
+        // ====================================================
+
         static LoggerManager& getInstance(){
+
             static LoggerManager instance;
+
             return instance;
         }
 
-        // destructor
-        ~LoggerManager() {
-            for(auto i: loggers){
+
+        // ====================================================
+        // DESTRUCTOR
+        // ====================================================
+
+        ~LoggerManager(){
+
+            for(auto i : loggers){
+
                 delete i.second;
             }
         }
 
+
+        // ====================================================
+        // GET LOGGER
+        // ====================================================
+
         Logger* getLogger(string name){
 
             if(loggers.find(name) != loggers.end()){
+
                 return loggers[name];
             }
 
-            // If logger with this name doesn't exist, create a new one
+
+            // ------------------------------------------------
+            // Create formatter
+            // ------------------------------------------------
 
             shared_ptr<LogFormatter> formatter =
                 make_shared<defaultFormatter>();
+
+
+            // ------------------------------------------------
+            // Create Logger
+            // ------------------------------------------------
 
             Logger* newLogger =
                 new Logger(
@@ -219,35 +506,80 @@ class LoggerManager {
                     formatter
                 );
 
+
+            // ------------------------------------------------
+            // Create console appender
+            // ------------------------------------------------
+
             shared_ptr<Appender> console =
                 make_shared<consoleAppender>();
 
+
             newLogger->addAppender(console);
 
+
+            // ------------------------------------------------
+            // Store Logger
+            // ------------------------------------------------
+
             loggers[name] = newLogger;
+
 
             return newLogger;
         } 
 };
+
+
+// ============================================================
+// MAIN
+// ============================================================
 
 int main(){
 
     LoggerManager& loggerManager =
         LoggerManager::getInstance();
 
+
     Logger* logger =
         loggerManager.getLogger("MyLogger");
 
-    logger->info("This is an info message");
 
-    logger->debug("This is a debug message");   
+    logger->info(
+        "This is an info message"
+    );
 
-    logger->warn("This is a warning message");
+    logger->debug(
+        "This is a debug message"
+    );   
 
-    logger->error("This is an error message");
+    logger->warn(
+        "This is a warning message"
+    );
 
-    logger->fatal("This is a fatal message");
+    logger->error(
+        "This is an error message"
+    );
+
+    logger->fatal(
+        "This is a fatal message"
+    );
+
+
+    /*
+        DO NOT:
+
+        delete logger;
+        delete &loggerManager;
+
+        LoggerManager owns the Logger.
+        LoggerManager's destructor will delete it.
+
+        And Logger's destructor will:
+            1. stop accepting new logs
+            2. wake worker
+            3. process remaining logs
+            4. join worker
+    */
 
     return 0;
 }
-
